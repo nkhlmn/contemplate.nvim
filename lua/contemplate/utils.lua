735a48a6ff0c949073dbd03ccc17794a3dc51580
Nikhil <nikhil.kamineni@gmail.com>
2022-10-18 19:54:24 -0700

    Use ISO 8601 standard for timestamps

--- lua/contemplate/utils.lua
+++ lua/contemplate/utils.lua
@@ -1,63 +1,66 @@
 local M = {}
 
 function M.split_lines(contents)
-	local lines = {}
-	for line in string.gmatch(contents, "(.-)\n") do
-		table.insert(lines, line)
-	end
-	return lines
+  local lines = {}
+  for line in string.gmatch(contents, "(.-)\n") do
+    table.insert(lines, line)
+  end
+  return lines
 end
 
 function M.get_file_contents(file_path)
-	if file_path == nil then
-		return ""
-	end
+  if file_path == nil then
+    return ""
+  end
 
-	local file = io.open(file_path)
-	if file ~= nil then
-		return file:read("a")
-	else
-		return ""
-	end
+  local file = io.open(file_path)
+  if file ~= nil then
+    return file:read("a")
+  else
+    return ""
+  end
 end
 
 function M.get_file_lines(file_path)
-	local file_contents = M.get_file_contents(file_path)
-	return M.split_lines(file_contents)
+  local file_contents = M.get_file_contents(file_path)
+  return M.split_lines(file_contents)
 end
 
 function M.is_filename(arg)
-	return arg:match("%.%a+$") ~= nil
+  return arg:match("%.%a+$") ~= nil
 end
 
 function M.get_timestamp_prefix()
-	local current_date = os.date("*t")
-	local output = string.format(
-		"%02d-%02d-%02d_%02d%02d%02d",
-		current_date.month,
-		current_date.day,
-		string.sub(tostring(current_date.year), 3, 4),
-		current_date.hour,
-		current_date.min,
-		current_date.sec
-	)
-	return output
+  local current_date = os.date("*t")
+  local output = string.format(
+    "%02d%02d%02dT%02d%02d%02d",
+    current_date.year,
+    current_date.month,
+    current_date.day,
+    current_date.hour,
+    current_date.min,
+    current_date.sec
+  )
+  return output
 end
 
 function M.get_temp_filename(opts)
-	local timestamp_prefix = M.get_timestamp_prefix()
+  local timestamp = M.get_timestamp_prefix()
+
   if opts.name and opts.file_extension then
-		return string.format("%s-%s.%s", timestamp_prefix, opts.name, opts.file_extension)
+    return string.format("%s-%s.%s", opts.name, timestamp, opts.file_extension)
   elseif opts.filename ~= nil then
-		return string.format("%s-%s", timestamp_prefix, opts.filename)
+    local extension = string.match(opts.filename, "%w+.(%w+)$")
+    local name = string.match(opts.filename, "(%w+).%w+$")
+    return string.format("%s-%s.%s", name, timestamp, extension)
   elseif opts.filename and opts.name ~= nil then
     local file_extension = string.match(opts.filename, "%w+.(%w+)$")
-		return string.format("%s-%s.%s", timestamp_prefix, opts.name, file_extension)
-	elseif opts.file_extension then
-		return string.format("%s.%s", timestamp_prefix, opts.file_extension)
-	else
-		error("Must provide either a filename or file_extension")
-	end
+    return string.format("%s-%s.%s", opts.name, timestamp, file_extension)
+  elseif opts.file_extension then
+    return string.format("%s.%s", timestamp, opts.file_extension)
+  else
+    error("Must provide either a `filename` or `file_extension`")
+  end
 end
 
 return M
